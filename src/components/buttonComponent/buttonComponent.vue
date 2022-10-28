3e55c6e91a188c48baf6caf0df702410beac938c
Anna Popova <61383401+anna-popova@users.noreply.github.com>
2022-10-28 19:39:00 +0300

    Update buttonComponent.vue

--- src/components/buttonComponent/buttonComponent.vue
+++ src/components/buttonComponent/buttonComponent.vue
@@ -1,10 +1,21 @@
 <template>
-  <button class="button">ADD</button>
+  <button class="button" @click="onClick">{{text}}</button>
 </template>
 
 <script>
 export default {
   name: 'buttonComponent',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
 };
 </script>
 
