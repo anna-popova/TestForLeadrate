827541416d1dca218b08d7e907037e33fdde73a2
Anna Popova <61383401+anna-popova@users.noreply.github.com>
2022-10-28 19:39:32 +0300

    Update checkboxComponent.vue

--- src/components/checkboxComponent/checkboxComponent.vue
+++ src/components/checkboxComponent/checkboxComponent.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="checkbox-list">
-    <label class="checkbox-list__item">
+    <label class="checkbox-list__item" :class="{ 'checked': checked }">
       <input
         :checked="checked"
         type="checkbox"
         class="checkbox-list__item--input"
-        :class="{ 'checked': checked }"
         @input="onInput"
       />
-      <div>
+      <div class="checkbox-list__item--data">
         <span class="checkbox-list__item--text">{{title}}</span>
         <span v-if="subtitle" class="checkbox-list__item--text checkbox-list__item--text_subtitle">
           {{subtitle}}
